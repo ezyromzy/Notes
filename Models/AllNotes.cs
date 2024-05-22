78c5c9e76980c74018dc68395c1915989f6969dd
ezyromzy <roma.kocnev12345@gmail.com>
2024-05-22 19:19:40 +0300

    Add AllNotes.cs (#13)

---
+++ Models/AllNotes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Notes.Models
+{
+    internal class AllNotes
+    {
+        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+        public AllNotes() =>
+            LoadNotes();
+
+        public void LoadNotes()
+        {
+            Notes.Clear();
+
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            IEnumerable<Note> notes = Directory
+                                        .EnumerateFiles(appDataPath, "*.notes.txt")
+                                        .Select(fileName => new Note()
+                                        {
+                                            FileName = fileName,
+                                            Text = File.ReadAllText(fileName),
+                                            Date = File.GetLastWriteTime(fileName)
+                                        })
+                                        .OrderBy(note => note.Date);
+
+            foreach (Note note in notes)
+                Notes.Add(note);
+        }
+
+    }
+}
