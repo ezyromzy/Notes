3156e1512bfb6f5348ee2733798be11f0c25cfed
ezyromzy <roma.kocnev12345@gmail.com>
2024-05-24 14:20:31 +0300

    Update NotesVM.cs, AllNotesPage.xaml and AllNotesPage.xaml.cs (#20)

--- ViewModels/NotesViewModel.cs
+++ ViewModels/NotesViewModel.cs
@@ -1,6 +1,55 @@
-﻿namespace Notes.ViewModels
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Notes.Models;
+using Notes.Views;
+
+namespace Notes.ViewModels
 {
-    internal class NotesViewModel
+    internal class NotesViewModel : IQueryAttributable
     {
+        public ObservableCollection<NoteViewModel> AllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+
+        public NotesViewModel()
+        {
+            AllNotes = new ObservableCollection<NoteViewModel>(Note.LoadAll().Select(n => new NoteViewModel(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<NoteViewModel>(SelectNoteAsync);
+        }
+
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(NotePage));
+        }
+
+        private async Task SelectNoteAsync(NoteViewModel note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(NotePage)}?load={note.Identifier}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                if (matchedNote != null)
+                    AllNotes.Remove(matchedNote);                  
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                if (matchedNote != null)
+                    matchedNote.Reload();
+                else
+                    AllNotes.Add(new NoteViewModel(Note.Load(noteId)));
+            }
+        }
     }
 }
